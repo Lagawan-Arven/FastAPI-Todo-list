25a56886d31739c814a1759689cded283ec979d1
Arven Lagawan <arvenlagawan0731@gmail.com>
2025-12-06 21:30:47 +0800

    more update

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
@@ -12,26 +12,93 @@ app.add_middleware(
     allow_headers = ["*"]
 )
 
+global_todo_id  = 1
+
 class Todo(BaseModel):
+    id: int
     name: str
     priority: str
     difficulty: str
     status: str
 
+class Todo_create(BaseModel):
+    name: str
+    priority: str
+    difficulty: str
+    status: str   
+
 todos: list[Todo] = []
 
 @app.get("/todos")
 def get_todos():
-    return todos
+    if todos == []:
+        return "There is no todo yet!"
+    else:
+        return todos
+
+@app.get("/todos/search")
+def get_searched_todos(q: str):
+    searched_todos = []
+    try:
+        for t in todos:
+            if(
+                q.lower() in t.name.lower() or
+                q.lower() in t.priority.lower() or
+                q.lower() in t.difficulty.lower() or
+                q.lower() in t.status.lower()
+            ):
+                searched_todos.append(t) 
+
+        if searched_todos:
+            return searched_todos
+        else:
+            return "Search not found!"
+    except:
+        return "No todo yet!"
 
 @app.post("/todos")
-def add_todo(todo: Todo):
-    todos.append(todo)
-    return {"message":"todo added succcessfully!"}
+def add_todo(todo_input: Todo_create):
+    global global_todo_id
+    todo = Todo(
+        id = global_todo_id,
+        name = todo_input.name,
+        priority = todo_input.priority,
+        difficulty = todo_input.difficulty,
+        status = todo_input.status
+    )
 
-@app.delete("/todos/{todo_name}")
-def delete_todo(todo_name: str):
+    todos.append(todo)
+    auto_update_id()
+    global_todo_id += 1
+
+    return "Todo added succcessfully!"
+
+def auto_update_id():
+    local_todo_id = 1
+    i = 0
     for t in todos:
-        if t.name == todo_name:
+        todos[i].id = local_todo_id
+        i+=1
+        local_todo_id+=1
+
+@app.post("/todos/{todo_id}")
+def update_todo(todo_id: int,todo_update: Todo_create):
+    for t in todos:
+        if t.id == todo_id:
+            t.name = todo_update.name
+            t.priority = todo_update.priority
+            t.difficulty = todo_update.difficulty
+            t.status = todo_update.status
+            return "Todo updated!"
+    raise HTTPException(status_code=404,detail="Todo not found!")   
+        
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int):
+    for t in todos:
+        if t.id == todo_id:
             todos.remove(t)
-            return {"message":"todo deleted!"}
+            auto_update_id()
+            return "Todo deleted!"
+    
+    raise HTTPException(status_code=404,detail="Todo not found!")    
